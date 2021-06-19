6bc109523dc66795ab1b0ae33f9c49c6459756d4
Julio <julio@ctrlsun.com>
2021-06-19 13:06:28 -0500

    Added GET and POST capabilities, part 5.

--- apps/api/api.py
+++ apps/api/api.py
@@ -1,4 +1,4 @@
-from rest_framework.views import APIView
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -6,7 +6,7 @@ from apps.users.models import User
 from apps.api.serializers import UserSerializer
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def user_api_view(request):
     """User list view."""
 
@@ -15,3 +15,10 @@ def user_api_view(request):
         user_serializer = UserSerializer(user, many=True)
 
         return Response(user_serializer.data)
+    elif request.method == "POST":
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+
+            return Response(user_serializer.data)
+        return Response(user_serializer.errors)
