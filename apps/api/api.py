93ac5da4f8601b3277481292e791d75d59d5bc26
Julio <julio@ctrlsun.com>
2021-06-19 13:42:41 -0500

    Added GET, PUT, DELETE, part 6.

--- apps/api/api.py
+++ apps/api/api.py
@@ -1,6 +1,7 @@
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 from apps.users.models import User
 from apps.api.serializers import UserSerializer
@@ -22,3 +23,30 @@ def user_api_view(request):
 
             return Response(user_serializer.data)
         return Response(user_serializer.errors)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def user_detail_api_view(request, pk=None):
+    """User detail view."""
+
+    user = User.objects.filter(id=pk).first()
+
+    if request.method == "GET":
+        user_serializer = UserSerializer(user)
+
+        return Response(user_serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == "PUT":
+        user_serializer = UserSerializer(user, data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        user.delete()
+        return Response(
+            {"message": "Usuario eliminado correctamente"},
+            status=status.HTTP_200_OK
+        )
--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from apps.api.api import user_api_view
+from apps.api.api import user_api_view, user_detail_api_view
 
 app_name = "api"
 
 urlpatterns = [
     path("users/", user_api_view, name="users"),
+    path("users/<int:pk>/", user_detail_api_view, name="user-detail"),
 ]
